1e83192df2aaa1ed646488908cd64da65b948cbb
ekobw <ekobw.py@gmail.com>
2024-12-08 11:12:13 +0700

    Add project files.

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        string musik = "";
+        string film = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            musik = "";
+            film = "";
+
+            if (checkBox1.Checked)
+            {
+                musik = "pop";
+            }
+
+            if (checkBox2.Checked)
+            {
+                if (!string.IsNullOrEmpty(musik))
+                {
+                    musik += ", dangdut";
+                }
+                else
+                {
+                    musik = "dangdut";
+                }
+            }
+
+            if (radioButton1.Checked)
+            {
+                film = "Action";
+            }
+            else if (radioButton2.Checked)
+            {
+                film = "Drama";
+            }
+
+            string message = "";
+
+            if (!string.IsNullOrEmpty(musik))
+            {
+                message += "Anda menyukai musik jenis " + musik;
+            }
+
+            if (!string.IsNullOrEmpty(film))
+            {
+                if (!string.IsNullOrEmpty(message))
+                {
+                    message += " dan ";
+                }
+                message += "film bergenre " + film + ".";
+            }
+
+            if (string.IsNullOrEmpty(musik) && string.IsNullOrEmpty(film))
+            {
+                MessageBox.Show("Anda belum memilih musik atau film.");
+            }
+            else if (string.IsNullOrEmpty(musik))
+            {
+                MessageBox.Show("Anda menyukai film bergenre " + film + ".");
+            }
+            else if (string.IsNullOrEmpty(film))
+            {
+                MessageBox.Show("Anda menyukai musik jenis " + musik + ".");
+            }
+            else
+            {
+                MessageBox.Show(message);
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+    }
+}
---
+++ WindowsFormsApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Forms;
+
+// Add this line
+using WindowsFormsApp1;
+
+namespace WindowsFormsApp1
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
